80381429e2ff57332c6074d6c9b44686096792ad
Chips-pixel <not.mrchips.16@gmail.com>
2024-10-16 21:42:58 +0500

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,1 +1,28 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
 
+async function scrapeLeads(url) {
+    try {
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
+        const leads = [];
+
+        $('a[href^="mailto:"]').each((index, element) => {
+            leads.push($(element).attr('href').replace('mailto:', ''));
+        });
+
+        $('a[href*="instagram.com"]').each((index, element) => {
+            leads.push($(element).attr('href').split('/').pop());
+        });
+
+        return leads;
+    } catch (error) {
+        console.error(`Error scraping ${url}:`, error);
+        return [];
+    }
+}
+
+(async () => {
+    const leads = await scrapeLeads('https://example.com');
+    console.log('Leads:', leads);
+})();
